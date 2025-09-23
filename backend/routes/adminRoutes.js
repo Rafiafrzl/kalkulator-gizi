befc599f68ab2961e4a1fda15d95f1860d36331a
Rafiafrzl <mrafiafrizal0@gmail.com>
2025-09-23 11:17:45 +0700

    update admin routes

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllUsers, getAllHistories, getStats, deleteUser, deleteHistory } from "../controllers/adminController.js";
+import { getAllUsers, getAllHistories, deleteUser, deleteHistory } from "../controllers/adminController.js";
 import { protect } from "../middleware/authMiddleware.js";
 import { isAdmin } from "../middleware/adminMiddleware.js";
 
@@ -12,8 +12,5 @@ router.delete("/users/:id", protect, isAdmin, deleteUser);
 // Manajemen Riwayat
 router.get("/histories", protect, isAdmin, getAllHistories);
 router.delete("/histories/:id", protect, isAdmin, deleteHistory);
-
-// Statistik
-router.get("/stats", protect, isAdmin, getStats);
 
 export default router;
